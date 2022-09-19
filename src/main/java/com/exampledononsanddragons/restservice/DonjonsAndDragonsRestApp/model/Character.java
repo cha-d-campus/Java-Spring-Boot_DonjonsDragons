ff432b1acc3b3c03145d14985996710090014d3e
Charlène DAUTUN <charlene.dautun@Digital-Grenoble.local>
2022-09-19 16:56:48 +0200

    affichage de la liste des personnages en d'un personnage par id en JSON + gestion d'URI

---
+++ src/main/java/com/exampledononsanddragons/restservice/DonjonsAndDragonsRestApp/model/Character.java
@@ -0,0 +1,60 @@
+package com.exampledononsanddragons.restservice.DonjonsAndDragonsRestApp.model;
+
+public class Character {
+    private int id;
+    private String name;
+    private String type;
+    private int lifepoints;
+
+    public Character() {
+    }
+
+    public Character(int id, String name, String type, int lifepoints) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.lifepoints = lifepoints;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getLifepoints() {
+        return lifepoints;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setLifepoints(int lifepoints) {
+        this.lifepoints = lifepoints;
+    }
+
+    @Override
+    public String toString() {
+        return "Character{" +
+                "id=" + id +
+                ", nom='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", points de vie" + lifepoints +
+                '}';
+    }
+}
--- src/main/java/com/exampledononsanddragons/restservice/DonjonsAndDragonsRestApp/web/controller/CharacterController.java
+++ src/main/java/com/exampledononsanddragons/restservice/DonjonsAndDragonsRestApp/web/controller/CharacterController.java
@@ -1,12 +1,42 @@
 package com.exampledononsanddragons.restservice.DonjonsAndDragonsRestApp.web.controller;
 
+import com.exampledononsanddragons.restservice.DonjonsAndDragonsRestApp.model.Character;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @RestController
 public class CharacterController {
-    @GetMapping("/Characters")
-    public String listCharacters() {
-        return "Un exemple de personnage !";
+    List <Character> listCharacter = List.of(
+            new Character(5, "Lux", "Magicien", 4),
+            new Character(2, "Garen", "Guerrier", 9),
+            new Character(9, "Veigar", "Magicien", 5),
+            new Character(9, "Morgana", "Magicien", 6),
+            new Character(42, "Sett", "Guerrier", 8)
+    );
+
+    @GetMapping("/characters")
+    public List <Character> listCharacters() {
+        return listCharacter;
+    }
+    @GetMapping("/characters/{id}")
+    public Character displayACharacter(@PathVariable int id) {
+        List<Character> characters = listCharacter
+                .stream()
+                .filter(x -> id == x.getId())
+                .toList();
+        if(characters.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        if(characters.size()!=1){
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return characters.get(0);
     }
 }
